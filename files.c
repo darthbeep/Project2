1ce205e4c2c9bd1cdb703e164362d956a9468fd8
MatthewSo <mso@stuy.edu>
2017-01-17 22:43:07 -0500

    added server shared memory creation

---
+++ files.c
@@ -0,0 +1,29 @@
+#include <sys/shm.>
+#include <sys/ipc.h>
+#include <sys/types.h>
+
+
+int serverOpenMemory(char* fileName){
+  key_t key;
+  int shmid;
+  char *data;
+  int mode;
+  
+  if ((key = ftok(fileName, 'R')) == -1) 
+    {
+      perror("ftok");
+      exit(1);
+    }
+
+  if ((shmid = shmget(key, 2048, 0644 | IPC_CREAT)) == -1) {
+    perror("shmget");
+    exit(1);
+  }
+  data = shmat(shmid, (void *)0, 0);
+  if (data == (char *)(-1)) {
+    perror("shmat");
+    exit(1);
+  }
+  
+  return 0;
+}
